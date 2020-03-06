76199a8cb7bd928c536267f46e8a86a9c773bcba
Jack Zhang <dvorjackz@gmail.com>
2020-03-05 17:57:33 -0800

    Jupyter notebook + jupytext

---
+++ Moneymaker.py
@@ -0,0 +1,19 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.3.4
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+# # RNN Moneymaker
+
+import numpy as np
+print("Edward is poor.")
